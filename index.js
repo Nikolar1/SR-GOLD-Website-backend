d5101511cf9b2139d1d7e97283764ffa0b4a6e44
Nikola Radojcic <nikolajazak@gmail.com>
2018-08-02 08:49:55 +0200

    Promenjen odabir porta

--- index.js
+++ index.js
@@ -124,4 +124,9 @@ app.get('/Ceno', function(req, res){
   })
 })
 
-app.listen(port);
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
