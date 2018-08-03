9a6b749abef8f0d62ecd4adb108b01d3ae04b2d4
Nikola Radojcic <nikolajazak@gmail.com>
2018-08-03 11:36:04 +0200

    Dodao cross-origin support

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ var Objava = require('./model/sema');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function(req, res){
   mongoose.connect(uri, function(err){
